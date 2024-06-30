8b4690fe7ebdce3d5e7961828709304f91755d58
Mark <98974230+Kryp7os@users.noreply.github.com>
2024-06-30 11:51:34 -0400

    Create bprips.sh

---
+++ bprips.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+############################
+##  Methods
+############################
+prefix_to_bit_netmask() {
+    prefix=$1
+    shift=$(( 32 - prefix ))
+
+    bitmask=""
+    for (( i=0; i < 32; i++ )); do
+        num=0
+        if [ $i -lt $prefix ]; then
+            num=1
+        fi
+
+        space=
+        if [ $(( i % 8 )) -eq 0 ]; then
+            space=" ";
+        fi
+
+        bitmask="${bitmask}${space}${num}"
+    done
+    echo $bitmask
+}
+
+expand_cidr() {
+    local cidr=$1
+    local ip=$(echo $cidr | cut -d '/' -f 1)
+    local prefix=$(echo $cidr | cut -d '/' -f 2)
+    local IFS=.
+    local -a octets=($ip)
+    local bin_ip=""
+
+    for octet in ${octets[@]}; do
+        bin_ip+=$(echo "obase=2; $octet" | bc | awk '{printf "%08d", $0}')
+    done
+
+    local num_ips=$(( 1 << (32 - prefix) ))
+    local range_start=$(echo "ibase=2; $bin_ip" | bc)
+    local range_end=$((range_start + num_ips - 1))
+
+    for (( ip=range_start; ip<=range_end; ip++ )); do
+        local ip1=$((ip>>24 & 0xFF))
+        local ip2=$((ip>>16 & 0xFF))
+        local ip3=$((ip>>8 & 0xFF))
+        local ip4=$((ip & 0xFF))
+        echo "${ip1}.${ip2}.${ip3}.${ip4}"
+    done
+}
+
+#############################
+##  MAIN
+#############################
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Usage: $0 -f <inputfile>"
+    exit 1
+fi
+
+while getopts ":f:" opt; do
+    case $opt in
+        f)
+            file="$OPTARG"
+            if [ ! -f "$file" ]; then
+                echo "File not found: $file"
+                exit 1
+            fi
+            while IFS= read -r line; do
+                if echo "$line" | grep -q '/'; then  # Check if the line contains a CIDR notation
+                    expand_cidr "$line"
+                fi
+            done < "$file"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
